7df686b9a05327f29d290eaf09d6bb659e8334e6
rahul-sani <sani.rahul999@gmail.com>
2020-05-17 09:18:31 -0700

    update Password added

---
+++ php/changePass.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+$uname = $_SESSION['username'];
+$pass1 = $_GET['pass1'];
+$pass2 = $_GET['pass2'];
+
+if($pass1==$pass2 and isset($uname)){
+    $servername = "localhost";
+    $username = "r4hu1";
+    $dbpassword = "Sani_1234";
+    $dbname = "airport_management"; 
+
+    $connection = mysqli_connect($servername, $username,$dbpassword,$dbname);
+
+    if(!$connection) {
+        die("Connection failed : " . mysqli_connect_error());
+    } else {
+        $sql = "UPDATE users set password='$pass1' WHERE uname='$uname' ";
+        $result = mysqli_query($connection, $sql);
+
+        echo "<script>
+        alert('Successfully changed password!');
+        window.location.href='home.php';
+        </script>";
+        
+    }
+
+} else {
+    echo "diff";
+}
+?>
