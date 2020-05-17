d23cbf8255c33ecadfdbf9aa7c1a2bd785ba0a54
rahul-sani <sani.rahul999@gmail.com>
2020-05-17 09:17:20 -0700

    added session management

--- php/signin.php
+++ php/signin.php
@@ -16,21 +16,24 @@ if(isset($_POST['login'])) {
     } 
     else {
         $sql = "SELECT uname FROM users where uname = '$uname' and password= '$password' ";
+        setcookie('sql',$sql,time()+3600);
         $result = mysqli_query($connection, $sql);
 
         if (mysqli_num_rows($result) != 0){
             $row = mysqli_fetch_assoc($result);
             
             setcookie('username',$row['uname'],time()+3600);
+            // setcookie('pass',$password,time()+3600);
 
             session_start();
-            $_SESSION['username'] = $row['name'];
+            $_SESSION['username'] = $row['uname'];
+            $_SESSION['count'] = 0;
 
             if($row['uname'] == $admin) {
                 header('location: adminDboard.php');
             }else {
                 
-                header('location: home.html');
+                header('location: home.php');
             }
             
         } else {
