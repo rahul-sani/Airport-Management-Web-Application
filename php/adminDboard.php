1698778df80cbad48f5fbf329ecac1e526a39943
X <jd.sathyan.0013@gmail.com>
2020-05-17 21:05:53 +0530

    allo

--- php/adminDboard.php
+++ php/adminDboard.php
@@ -2,23 +2,33 @@
 <html>
 <head>
 	<title>Admin Dashboard</title>
-	<meta charset="utf-8"
+	<meta charset="utf-8">
 	 <meta name="viewport" content="width=device-width, initial-scale=1" />
-	 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Asul" >
-	<link rel="stylesheet" type="text/css" href="../css/styleC.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
 
 	<header>
 		<h1><span id="plane">&#9992</span> Airport Management System</h1>
+		<img id="loginicon" src="../image/user.svg">
+		<br>
+		<a id="logoutbutton" href="logout.php">Logout?</a>
+		<p id="logintext">Hello Admin</p>
 	</header>
-	<div class="main">
-		<a id="passengers" href=".html"><button type="button" class="admbtn">Passengers</button></a>
-		<a id="flightchart" href=".html"><button type="button" class="admbtn">Flight Chart</button></a>
-		<a id="users" href=".html"><button type="button" class="admbtn">Users</button></a>
-		<a id="report" href=".html"><button type="button" class="admbtn">Generate Report</button></a><br><br>
-		<a id="logout" href="logout.php"><button type="button" class="admbtn">Logout</button></a>		
-		
+	<div class="main">	
+
+		<h1>Admin Home</h1>		
+		<ul class="menu">
+			<li class="menuitem">
+				<a href="../report.html">Passengers Report</a>
+			</li class="menuitem">
+			<li class="menuitem"> 
+				<a href="../report.html">Flight Chart Report</a>	
+			</li>
+			<li class="menuitem">
+				<a href="../report.html">Accounts Report</a>
+			</li>			
+		</ul>
 	</div>
 	<footer>
 		<h1>Software engineering project</h1>
