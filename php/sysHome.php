3319d7b8a35805b3abc991d561a0b5466179876f
X <jd.sathyan.0013@gmail.com>
2020-05-18 19:23:29 +0530

    a

--- php/sysHome.php
+++ php/sysHome.php
@@ -28,7 +28,7 @@ else {
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<!-- <script type="text/javascript">
-		document.addEventListener("DOMContentLoaded", function(){;
+		document.addEventListener("DOMContentLoaded", function(){
 		document.getElementById("logintext").innerHTML="hello "+username;});
 	</script> -->
 </head>
